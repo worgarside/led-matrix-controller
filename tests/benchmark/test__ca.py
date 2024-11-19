93207781f98963c6744dea506be863f79a594fa4
Will Garside <worgarside@gmail.com>
2024-11-19 20:33:55 +0000

    Update test params

--- tests/benchmark/test__ca.py
+++ tests/benchmark/test__ca.py
@@ -13,8 +13,8 @@ if TYPE_CHECKING:
     from content.automaton import MaskGen
     from pytest_codspeed import BenchmarkFixture  # type: ignore[import-untyped]
 
-SIZES = [32, 64]
-LIMITS = [50, 200]
+SIZES = [32]
+LIMITS = [200]
 
 
 @pytest.mark.parametrize(
