7e8fc5f21c402e46295a76a984139eb6bbc720dd
Will Garside <worgarside@gmail.com>
2024-11-30 20:27:00 +0000

    fix

--- led_matrix_controller/content/audio_visualiser.py
+++ led_matrix_controller/content/audio_visualiser.py
@@ -183,7 +183,7 @@ class AudioVisualiser(DynamicContent):
         while self.active:
             if self._refresh_audio_array:
                 audio: NDArray[np.float64] = np.ndarray(
-                    shape=(self.chunk_size // 2 + 3,),
+                    shape=(self.chunk_size // 2 + 4,),
                     dtype=np.float64,
                     buffer=self.shm.buf,
                 )
