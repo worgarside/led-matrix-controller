52592a46cdbbd3a3d8743900ea2058221cc15740
Will Garside <worgarside@gmail.com>
2025-07-06 17:13:18 +0100

    bump queue size to 20

--- led_matrix_controller/content/snake.py
+++ led_matrix_controller/content/snake.py
@@ -69,7 +69,7 @@ class SnakeDirection(enum.Enum):
 class Snake(Automaton):
     """Basic Snake game simulation."""
 
-    QUEUE_SIZE: ClassVar[int] = 5
+    QUEUE_SIZE: ClassVar[int] = 20
 
     TRACK_STATES_DURATION: ClassVar[tuple[int, ...]] = (
         State.HEAD.state,
